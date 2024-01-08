d041f857704edb7c65cb926c8232a0a3d7686381
DenisSkendaj <imdenisskendaj@gmail.com>
2024-01-08 16:57:22 +0000

    [fix]: vercel issue, remove ise screenshot feature

--- src/ui/ModelContent.tsx
+++ src/ui/ModelContent.tsx
@@ -1,10 +1,3 @@
-import { useState } from 'react';
-
-import { useClipboard } from '@/hooks/useClipBoard';
-import { useScreenShot } from '@/hooks/useScreenShot';
-import { IoCopy } from 'react-icons/io5';
-import { FaCameraRetro } from 'react-icons/fa';
-
 import Character from './Character';
 import ResultCard from './ResultCard';
 
@@ -27,18 +20,12 @@ type ModalContentProps = {
 };
 
 export default function ModalContent({ totalTime, history, results }: ModalContentProps) {
-    const [copied, setCopied] = useState(false);
-    const [imageCopied, setImageCopied] = useState(false);
-
-    const { copyTextToClipboard } = useClipboard();
-    const { ref, image, getImage } = useScreenShot();
 
     return (
         <div
         className='mx-auto flex h-full w-[95%] flex-col gap-10 pb-10 pt-8 font-mono text-[var(--text-color)]'
         >
         <div
-            ref={ref}
             className='flex-[3] px-5 py-7 bg-[var(--background)]'
         >
             <div className=' grid grid-flow-col grid-rows-6 justify-center gap-4 sm:grid-rows-4 sm:justify-normal lg:grid-rows-2 lg:justify-normal lg:gap-10 '>
@@ -94,31 +81,6 @@ export default function ModalContent({ totalTime, history, results }: ModalConte
         <div className='flex-[3] px-5'>
             <div className='flex items-center gap-2'>
             <h2 className='text-xl lg:text-2xl'>watch history</h2>
-            <button
-                onClick={async () => {
-                const isCopied = await copyTextToClipboard(history.typedHistory);
-                if (isCopied) {
-                    setCopied(true);
-                    setTimeout(() => {
-                    setCopied(false);
-                    }, 2000);
-                }
-                }}
-                className='hover:text-[var(--accent)]'
-            >
-                <IoCopy className='cursor-pointer text-xl' />
-            </button>
-            <div
-                className='rounded-md bg-[var(--darker-background)]'
-            >
-                {copied === true ? (
-                <span
-                    className='p-5 text-[var(--text-color)]'
-                >
-                    Copied ✅
-                </span>
-                ) : null}
-            </div>
             </div>
             <div className='mt-3 text-lg lg:text-xl'>
             {history.typedHistory.split('').map((char, index) => {
@@ -130,45 +92,6 @@ export default function ModalContent({ totalTime, history, results }: ModalConte
                 />
                 );
             })}
-            </div>
-        </div>
-
-        <div className='flex flex-[1] flex-col px-5'>
-            <div
-            className='group mt-auto flex cursor-pointer items-center gap-2 '
-            onClick={async () => {
-                try {
-                getImage();
-                const res = await fetch(image);
-                const data = await res.blob();
-                await navigator.clipboard.write([
-                    new ClipboardItem({ [data.type]: data }),
-                ]);
-
-                setImageCopied(true);
-                setTimeout(() => {
-                    setImageCopied(false);
-                }, 2000);
-                } catch (error) {
-                console.log(error);
-                }
-            }}
-            >
-            <FaCameraRetro className=' text-xl' />
-            <span className='text-lg hover:underline'>
-                Screenshot your results and share to your friends🔥
-            </span>
-            <div
-                className='rounded-md bg-[var(--darker-background)]'
-            >
-                {imageCopied === true ? (
-                <span
-                    className='p-5 text-center text-[var(--text-color)]'
-                >
-                    Image copied to clipboard 😊
-                </span>
-                ) : null}
-            </div>
             </div>
         </div>
         </div>
